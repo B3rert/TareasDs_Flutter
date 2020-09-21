df9342925bc31525d885a8cca23f5c8a71a95703
Robert L <robertmorales181@gmail.com>
2020-09-21 12:21:09 -0600

    Cambio colres por clase colores global

--- lib/src/pages/view_tasks.dart
+++ lib/src/pages/view_tasks.dart
@@ -1,20 +1,5 @@
 import 'package:flutter/material.dart';
-
-Map<int, Color> color = {
-  50: Color.fromRGBO(136, 14, 79, .1),
-  100: Color.fromRGBO(136, 14, 79, .2),
-  200: Color.fromRGBO(136, 14, 79, .3),
-  300: Color.fromRGBO(136, 14, 79, .4),
-  400: Color.fromRGBO(136, 14, 79, .5),
-  500: Color.fromRGBO(136, 14, 79, .6),
-  600: Color.fromRGBO(136, 14, 79, .7),
-  700: Color.fromRGBO(136, 14, 79, .8),
-  800: Color.fromRGBO(136, 14, 79, .9),
-  900: Color.fromRGBO(136, 14, 79, 1),
-};
-MaterialColor naranja = MaterialColor(0xFFDF9623, color);
-MaterialColor azul = MaterialColor(0xFF124994, color);
-MaterialColor azulPalido = MaterialColor(0xFF567aab, color);
+import 'package:front_tareasds/src/utils/colors.dart';
 
 class ViewTaskPage extends StatelessWidget {
   @override
@@ -24,7 +9,7 @@ class ViewTaskPage extends StatelessWidget {
         iconTheme: IconThemeData(
           color: Colors.white,
         ),
-        backgroundColor: azul,
+        backgroundColor: ColoresPropios.azul,
         title: Text(
           "Tareas",
           style: TextStyle(color: Colors.white),
