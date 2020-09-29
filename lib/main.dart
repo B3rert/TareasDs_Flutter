1a0dd92a64bf5885dc50bb43c923a39d075f3b00
Robert L <robertmorales181@gmail.com>
2020-09-28 18:04:59 -0600

    Texto justificado

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:front_tareasds/src/pages/file_picker_page.dart';
 import 'package:front_tareasds/src/pages/login_page.dart';
 import 'package:front_tareasds/src/pages/task_view_page.dart';
 import 'package:front_tareasds/src/utils/colors.dart';
@@ -18,6 +19,7 @@ class MyApp extends StatelessWidget {
         "/": (context) => LoginPage(), //FilePickerPage(), //Ruta a login_page
         //"/homePage": (context) => HomePage(),
         "/viewTask": (context) => TaskViewPage(),
+        "/selectFilePichker": (context) => FilePickerPage(),
       },
       initialRoute: '/',
     );
--- lib/src/pages/task_view_page.dart
+++ lib/src/pages/task_view_page.dart
@@ -129,11 +129,12 @@ class _TaskViewPageState extends State<TaskViewPage> {
 
   Widget mostrarTextoNormal(String texto) {
     return Container(
-      margin: EdgeInsets.only(left: 20.0, top: 5.0),
+      margin: EdgeInsets.only(left: 20.0, top: 5.0, right: 20.0),
       child: Text(
         texto,
         style: TextStyle(
             fontFamily: "Lato", fontSize: 18.0, fontWeight: FontWeight.w500),
+        textAlign: TextAlign.justify,
       ),
     );
   }
@@ -188,7 +189,7 @@ class _TaskViewPageState extends State<TaskViewPage> {
             ),
           ),
           RaisedButton(
-            onPressed: () {},
+            onPressed: () => Navigator.pushNamed(context, "/selectFilePichker"),
             color: ColoresPropios.azul,
             textColor: Colors.white,
             child: Text('Examinar',
